31be61b1a020e4b5c532708e6d0b5ce1cb7ddf5c
artem-collab <58644376+artem-collab@users.noreply.github.com>
2019-12-30 19:32:42 +0400

    Create sucks.py

---
+++ sucks.py
@@ -0,0 +1,6 @@
+from random import choice
+while True:
+    lol=choice('qwertyuiopasdfghjklzxcvbnmйцукенгшщзхфывапролджэячсмитьбю')
+    with open(lol,'w') as l:
+        l.write('''
+Ты лузер это вирус он засаряет память''')
